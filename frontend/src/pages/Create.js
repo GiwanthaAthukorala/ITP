a51f5c7f79a9da83eca985457b8472d6fdd96c5f
GiwanthaAthukorala <143745003+GiwanthaAthukorala@users.noreply.github.com>
2024-02-14 03:38:42 +0530

    Create Create.js

---
+++ frontend/src/pages/Create.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import WorkoutForm from '../componets/WorkoutForm';
+
+const Home = () => {
+ 
+
+ 
+
+  return (
+    <div className="Create">
+       <h1>Delivery Details</h1>
+        <WorkoutForm />
+    </div>
+  );
+}
+
+export default Home;
