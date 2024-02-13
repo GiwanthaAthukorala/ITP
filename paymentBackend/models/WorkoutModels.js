d760792fdcf2b9e26faab2ba771bfdac9806adf6
GiwanthaAthukorala <143745003+GiwanthaAthukorala@users.noreply.github.com>
2024-02-14 03:39:12 +0530

    Create WorkoutModels.js

---
+++ paymentBackend/models/WorkoutModels.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  phonenumber: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Workout', WorkoutSchema);
