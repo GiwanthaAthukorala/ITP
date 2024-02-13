1aae729cf55a77ebf2426b9cc15235a3ae6c5ae5
GiwanthaAthukorala <143745003+GiwanthaAthukorala@users.noreply.github.com>
2024-02-14 03:37:19 +0530

    Create Navbar.js

---
+++ frontend/src/componets/Navbar.js
@@ -0,0 +1,27 @@
+import {Link} from 'react-router-dom'
+
+const Navbar = ( )=> {
+
+    return(
+        <header>
+            <div className="container">
+
+                <Link to ="/">
+                    
+                <h1>NV BAR</h1>
+               
+                    
+
+                </Link>
+
+
+            </div>
+        </header> 
+    )
+
+
+
+
+}
+
+export default Navbar;
