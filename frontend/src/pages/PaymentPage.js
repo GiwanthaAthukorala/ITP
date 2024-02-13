bde23d6f5cdbe1c95609300fd0d4afa9423fa4f7
GiwanthaAthukorala <143745003+GiwanthaAthukorala@users.noreply.github.com>
2024-02-14 03:38:50 +0530

    Create PaymentPage.js

---
+++ frontend/src/pages/PaymentPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PaymentForm from '../componets/paymentForm';
+
+
+
+const PaymentPage = () => {
+  return (
+    <div className="Create">
+      <h1>Online Payment</h1>
+      <PaymentForm />
+    </div> 
+  );
+}
+
+export default PaymentPage;
